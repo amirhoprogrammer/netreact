ab4948f35a0254d6c7838697ebd02072d7367b87
amihoprogrammer <amirhanegareshi@gmail.com>
2024-03-31 11:09:01 +0330

    Add paragraph

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 function App() {
   const title = 'my new react project';
+  const like = 50;
 
 
   return (
     <div className="App">
       <div className="content">
-        <h1>{ title }</h1>
+        <h1>{title}</h1>
+        <p>Liked { like } times</p>
       </div>
     </div>
   );
