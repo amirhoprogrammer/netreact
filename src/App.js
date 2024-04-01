502d18bd6c3e3d4bf0961ba32576323525e4b703
amihoprogrammer <amirhanegareshi@gmail.com>
2024-04-01 11:25:54 +0330

    add  navbar and home

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 //import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'my new react project';
-  const like = 50;
+  //const title = 'my new react project';
+  //const like = 50;
   // const person = {name :'ali' ,age:20};
   const link = "https://forooshmobile.ir/";
 
 
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked { like } times</p>
+        <Home />
+        {/*<p>Liked { like } times</p>*/}
         {/*<p>Liked { person } times</p>*/}
-        <p>{ 10 }</p>
-        <p>{ "hello,liverpool" }</p>
-        <p>{["persepolis ", " liverpool ", 1, 2]}</p>
+        {/*<p>{ 10 }</p>*/}
+        {/*<p>{ "hello,liverpool" }</p>*/}
+        {/*<p>{["persepolis ", " liverpool ", 1, 2]}</p>*/}
         <p>{Math.random() * 20}</p>
         
         <a href={link}>forooshmobile</a>
